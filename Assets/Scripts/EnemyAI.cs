9c55ace827b50112d3cfa1d241c47876684906d1
alan.stewart@chi.ac.uk <alan.stewart@chi.ac.uk>
2024-09-30 15:56:18 +0100

    Added EnemyAI.cs which sees Enemy chase Player if Player gets within Enemy's aggroRange

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    //cached refs
+    [SerializeField] Transform target;
+    NavMeshAgent nMA;
+
+    //parameters
+    [SerializeField] float aggroRange = 5f;
+    float distanceToPlayer;
+
+    private void Start()
+    {
+        nMA = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        distanceToPlayer = Vector3.Distance(target.position, transform.position);
+
+        if(distanceToPlayer <= aggroRange)
+        {
+            nMA.SetDestination(target.transform.position);
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, aggroRange);
+    }
+}
